c899fe81f045743e67a6f549dfae03fcf4554764
arinagg-z <117895829+arinagg-z@users.noreply.github.com>
2023-08-28 11:47:13 +0530

    uploading assests

--- lib/widgets/VideoPlayer.dart
+++ lib/widgets/VideoPlayer.dart
@@ -1,50 +1,56 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class videoplayer extends StatefulWidget {
   final String videourl;
   final String icon;
-  const videoplayer({super.key, required this.videourl, required this.icon});
+  videoplayer({required this.videourl, required this.icon});
 
   @override
-  State<videoplayer> createState() => _videoplayerState();
+  _videoplayerState createState() => _videoplayerState();
 }
 
 class _videoplayerState extends State<videoplayer> {
-  late VideoPlayerController vp;
-  late Future<void> intialize;
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
 
   @override
   void initState() {
-    vp = VideoPlayerController.networkUrl(Uri(path: widget.videourl));
+    _controller = VideoPlayerController.networkUrl(
+      Uri.parse(
+          'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4'),
+    );
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    _controller.setLooping(true);
+    _controller.play();
+
     super.initState();
-    intialize = vp.initialize().then((value) {
-      vp.setLooping(true);
-      vp.play();
-      setState(() {});
-    });
   }
 
   @override
   void dispose() {
-    vp.dispose();
+    _controller.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: intialize,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return AspectRatio(
-                aspectRatio: vp.value.aspectRatio, child: VideoPlayer(vp));
-          } else {
-            return Image.asset(
-              widget.icon,
-              fit: BoxFit.cover,
-            );
-          }
-        });
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
